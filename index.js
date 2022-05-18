c889c6c2eb3628772a232160b4848568f61deef8
Thai Xiong <thai303@hotmail.com>
2022-05-18 11:19:33 -0600

    commit tree 09718a097b11814d52566734e0e52b3ea46e28a6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteustomer = "don't know";
+var bestCustomer
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteustomer = 'Mike'
+}
